3c26eba561412bd0b81c9576a0dba7a83ed4e61e
rakib <phi.rakib@gmail.com>
2023-02-26 12:38:20 +0600

    added page layout view

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Multi Step Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Laravel Multi Step Form</h2>
+
+        @yield('content')
+    </div>
+</body>
+</html>

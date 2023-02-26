a057b105c2ecd4bf35c5fd082f522a74d3bfc7dd
rakib <phi.rakib@gmail.com>
2023-02-26 12:39:00 +0600

    added route for multi step form

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(ProductController::class)->group(function(){
+    Route::get('products', 'index')->name('products.index');
+
+    Route::get('products/create-step-one', 'createStepOne')->name('products.create.step.one');
+    Route::post('products/create-step-one-post', 'postCreateStepOne')->name('products.create.step.one.post');
+
+    Route::get('products/create-step-two', 'createStepTwo')->name('products.create.step.two');
+    Route::post('products/create-step-two-post', 'postCreateStepTwo')->name('products.create.step.two.post');
+
+    Route::get('products/create-step-three', 'createStepThree')->name('products.create.step.three');
+    Route::post('products/create-step-three-post', 'postCreateStepThree')->name('products.create.step.three.post');
 });
+
